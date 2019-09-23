5832beb2e6e1f950825da488e934fc14b42116f7
Sharik Gurung <SGurung@spartaglobal.com>
2019-09-23 13:56:14 +0100

    Casted something into str() and int()

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -8,9 +8,9 @@
 
 name = input('What is your name?')
 last_name = input('What is your surname?')
-age = input('What is your age?')
+age = int(input('What is your age?'))
 eye_colour = input('What is the colour of your eye?')
 hair_colour = input('What is the colour of your hair?')
 print('Hello',name.capitalize(), f'{last_name.capitalize()}!', 'Welcome, your age is', f'{age},', 'your eyes are', eye_colour, 'and your hair is', hair_colour)
 print(f'Hello {name} {last_name}, Welcome!, your age is')
-print('You were born in', 2019 - int(age))
+print('You were born in', 2019 - age)
---
+++ venv/104_data_type_casting.py
@@ -0,0 +1,15 @@
+# casting
+# casting is when you change an object data type to a specific data type
+
+#string to integer
+my_var = '10'
+print(type(my_var))
+
+my_casted_var = int(my_var)
+print(type(my_casted_var))
+
+# Integer/Floats to string
+my_int_vaar = 14
+my_casted_str = str(my_int_vaar)
+
+print('my_casted_str:', type(my_casted_str))
