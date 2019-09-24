a23f4555c9b91a35eccda262b5d8c7698deb6ef1
Sharik Gurung <SGurung@spartaglobal.com>
2019-09-24 15:09:02 +0100

    did for and if loops

---
+++ 109_if_statements.py
@@ -0,0 +1,60 @@
+# Control Flow - If statements
+# # controls where the code is going to go
+# depending on the result of the evaluation that return true or false, it runs a block of code or not
+
+
+#syntax
+# if<condition>:
+    # block
+# elif <condition>:
+    # block
+    # do stuff
+# else:
+    # block
+
+# weather = 'snowie', 'stormy'
+# if weather == 'rainy':
+#     print('take an umbrella')
+# elif weather == 'snowie':
+#     print('wear boots and scarf')
+# else:
+#     print('take shades')
+
+# age = int(input('what is your age? '))
+# driver_license = False
+#
+# if age>=18 and driver_license == True:
+#     print(f'you are {age}. you are of a legal age to vote, drive, and buy drinks for yourself and someone who is over16')
+# elif age<16:
+#     print('You are too young, go back to school')
+# elif age is 17 and driver_license == False:
+#     print(f'you can drink, but will need an over 18 to buy it for you')
+#     print("you should consider getting a driver's license")
+# elif age is 17 and driver_license is True:
+#     print(f'you can drink, but will need an over 18 to buy it for you')
+#     print(f'you can also drive')
+# elif 17 > age >= 16:
+#     print(f'you can drink, but will need an over 18 to buy it for you')
+#     print(f'you can also drive')
+
+# check for rating for this movie:
+  ## universal -> everyone can watch
+  ## pg, General viewing, but some scenes may be unsuitable for young children
+  ## 12, Films classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult.
+  ## 15, No one younger than 15 may see a 15 film in a cinema.
+  ## 18, No one younger than 18 may see an 18 film in a cinema.
+
+rating = input('pick a rating: ').lower()
+if rating == 'pg':
+    print('General viewing, but some scenes may be unsuitable for young children')
+elif  rating == 'u' or rating == 'universal':
+    print('everyone can watch')
+elif rating == '12' or rating == '12a':
+    print('Films classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12.')
+    print('No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult.')
+elif rating == '15':
+    print('No one younger than 15 may see a 15 film in a cinema.')
+elif rating == '18':
+    print('No one younger than 18 may see an 18 film in a cinema.')
+else:
+    print('invalid rating')
