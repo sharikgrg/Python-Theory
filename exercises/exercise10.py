ec6e35317db318dbbc2bf28eb701334ad3bd8afc
Sharik Gurung <SGurung@spartaglobal.com>
2019-09-25 01:23:16 +0100

    loop assignments

--- exercises/exercise10.py
+++ exercises/exercise10.py
@@ -13,3 +13,37 @@
 # extra:
  # stick it in a while loop so we continue to listen to stories :)
  # It would be nice to be able to read only one story
+name = input('name a hero: '.capitalize())
+planet = input('Name a planet: '.capitalize())
+country = input('name a country: '.capitalize())
+verdict = input('died or survived? ')
+number = ('')
+
+story = {
+    'hero': name,
+    'beginning': country,
+    'middle': planet,
+    'end': verdict
+}
+while number != 4:
+    number = int(input('choose a number between 1, 2 and 3 to read the story or 4 to exit: '.upper())) ## pauses the loop
+    if number == 1:
+        print(f'There was a person called {story["hero"]}. He was living on a country called {story["beginning"]} on a planet called {story["middle"]}.')
+        print(f'He fought with his life and {story["end"]}')
+    elif number == 2:
+        print(f'{story["hero"]} was very stupid. Everyone from his country, {story["beginning"]},')
+        print(f'was stupid. Infact, the whole planet, called {story["middle"]}, was full of stupid.')
+        print(f'"surprisingly" the stupidness never {story["end"]} ')
+    elif number == 3:
+        print(f'{story["hero"]} had a little lamb. Everyone from his country, {story["beginning"]},')
+        print(f'had a little lamb. Actually their planet, {story["middle"]}, did not have little lamb.')
+        print(f'Those lambs were really massive. Hence, Everyone {story["end"]} ')
+    elif number == 4:
+        print('...Hope you enjoyed the stories...'.upper())
+        break
+    elif type(number)== int:
+        print('!!!you need to choose a number between 1, 2 or 3!!')
+
+# number = ''
+# while number != 1:
+#     number = input('enter either 1, 2 or 3 to read a story: ')
--- exercises/exercise9.py
+++ exercises/exercise9.py
@@ -8,4 +8,19 @@
     # YES! YESS! I WANT YOU TO BE MOTIVATED!! YES!
 # if your response is 'I'm a doctor'
     # WELLL DONE! YOU can now talk to me
-    # Exits
+    # Exit
+student = ''
+answer1 = "I'm a doctor"
+answer2 = str('')
+while student != answer1.lower():
+    student = input("what's up?? ")
+    if student.find('?') != -1:
+        print('HAHAHA! AHAHAHAHHA!! OMG! WHAT a silly question! Go back TO SCHOOL!')
+    elif student.find('!') != -1:
+        print('YES! YESS! I WANT YOU TO BE MOTIVATED!! YES!')
+    elif student == answer1.lower():
+        print('WELLL DONE! YOU can now talk to me')
+        break
+    elif type(student) == str:
+        print('Go back to school!')
+
